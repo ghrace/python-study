5bed359574aadaab25541e39a0372d6720d9db39
Hongrong Guo <ghrsevn@live.com>
2017-12-12 11:12:20 +0800

    Add files via upload
    os bs4 requests

---
+++ top.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+import os
+import requests
+from bs4 import BeautifulSoup
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+base_url = 'https://movie.douban.com/top250?start='  # 设定一个网址不变的部分，然后我们只要每次在这个后面加数字就可以了
+os.mkdir("img")
+os.chdir("img")
+for num in range(0,10):
+    page=str(num*25)
+    r=requests.get(base_url+page+'&filter=',headers=headers)
+    content = r.text
+    soup = BeautifulSoup(r.text, 'lxml')
+    divs=soup.find_all('img')
+    for div in divs:
+        title=div.get('alt')
+        src=div.get('src')
+        print(src)
+        photo=requests.get(src)
+        with open(title+".jpg", "wb") as f:
+            f.write(photo.content)
+
+# for num in range(0,10):
+#     page=str(num)
+#     r = requests.get(base_url+'-'+page, headers = headers) #这里对网址进行一个修改
+#     content = r.text
+#     # print(content)
+#     soup = BeautifulSoup(r.text, 'lxml')
+#     divs=soup.find_all('img')
+#     print(divs)
+#     for div in divs:
+#
+#         img=div.get('src')
+#         print(img)
+#         title=joke.get('alt')
+#         photo=requests.get(img)
+#
+#         with open(title+".jpg", "wb") as f:
+#             f.write(photo.content)
+#
+#         # print('------')
