6352cadf10f150afde74583280c47609b6b60d13
hongrong <ghrace@live.com>
2018-07-31 11:22:30 +0800

    xinhua

--- xinhua.py
+++ xinhua.py
@@ -1,11 +1,14 @@
 import requests
 # 新华字典库
 url='https://www.pwxcoo.com/dictionary'
-params={'type':'xiehouyu','riddle':'黄盖'}#歇后语 riddle 语面
-# params={'type':'idiom','riddle':'兴高采烈'}成语
-# params={'type':'idiom','riddle':'xgcl'} 拼音缩写
-# params={'type':'word','riddle':'王'} 汉字
-
+#歇后语 riddle 语面
+params={'type':'xiehouyu','riddle':'黄盖'}
+# 成语
+# params={'type':'idiom','riddle':'兴高采烈'}
+#  拼音缩写
+# params={'type':'idiom','riddle':'xgcl'}
+# 汉字
+# params={'type':'word','riddle':'王'} 
 r=requests.get(url=url,params=params)
 data=r.json()
 print(data)
