287faabd80f8d7ba81fd234d7422f2594dc364d3
hongrong <ghrace@live.com>
2018-09-14 18:30:46 +0800

    f

--- js/flat.js
+++ js/flat.js
@@ -4,3 +4,17 @@ arr1.flat();
 export function flatten (arr: Array<any>): Array<any> {
     return Array.prototype.concat.apply([], arr)
   }
+/**
+ * 空函数，什么都不做，用于初始化一些值为函数的变量。
+ */
+export function noop (a?: any, b?: any, c?: any) {}
+
+/**
+ * Always return false.
+ */
+export const no = (a?: any, b?: any, c?: any) => false
+
+/**
+ * Return same value
+ */
+export const identity = (_: any) => _
