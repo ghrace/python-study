60d116ce47158513feceba6d1d64d69e43413f15
hongrong <ghrace@live.com>
2018-09-19 16:31:06 +0800

    il

--- js/util.js
+++ js/util.js
@@ -379,27 +379,6 @@ function loadScript(url, callback) {
   document.body.appendChild(script);
 }
 
-function loadScript(url, callback) {
-  var script = document.createElement("script");
-  script.type = "text/";
-  if (typeof (callback) != "undefined") {
-    if (script.readyState) {
-      script.onreadystatechange = function () {
-        if (script.readyState == "loaded" || script.readyState == "complete") {
-          script.onreadystatechange = null;
-          callback();
-        }
-      };
-    } else {
-      script.onload = function () {
-        callback();
-      };
-    }
-  }
-  script.src = url;
-  document.body.appendChild(script);
-}
-
 function getRandomColor() {
   const rgb = []
   for (let i = 0; i < 3; ++i) {
