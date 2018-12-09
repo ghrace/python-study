cc1cd93e4d753de91e7ca54cb53ff1b2cc535459
郭洪荣 <ghr@ghrs-MBP.lan>
2018-12-09 14:20:37 +0800

    tsc

--- ts/hello.js
+++ ts/hello.js
@@ -1,3 +1,4 @@
+"use strict";
 // function sayHe(person:string){
 //     return 'hello' +person
 // }
@@ -11,35 +12,23 @@
 //         return `my name is ${this.name}`
 //     }
 // }
-// let a=new Animal('tom')
-// console.log(a.sayHi())
-// interface Person {
-//     name:string;
-//     age:number;
-// }
-// let tom: Person={
-//     name:'te',
-//     age:23
-// }
-// const AGE=23
-// class Animal{
-//     private name;
-//    public constructor(name){
-//        this.name=name
-//    }
-// }
-var Student = /** @class */ (function () {
-    function Student(firstName, middleInitial, lastName) {
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+}
+let a = new Animal('tom');
+class Student {
+    constructor(firstName, middleInitial, lastName) {
         this.firstName = firstName;
         this.middleInitial = middleInitial;
         this.lastName = lastName;
         this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
     }
-    return Student;
-}());
+}
 console.log(1 /* DOWN */);
 function greeter(person) {
     return 'hello,' + person.firstName + ' ' + person.lastName;
 }
-var user = new Student('jack', 'M.', 'john');
+let user = new Student('jack', 'M.', 'john');
 console.log(greeter(user));
--- ts/hello.ts
+++ ts/hello.ts
@@ -12,45 +12,35 @@
 //         return `my name is ${this.name}`
 //     }
 // }
+class Animal{
+    private name:string;
+   public constructor(name:string){
+       this.name=name
+   }
+}
+let a=new Animal('tom')
 
-// let a=new Animal('tom')
-// console.log(a.sayHi())
 
-// interface Person {
-//     name:string;
-//     age:number;
-// }
-
-// let tom: Person={
-//     name:'te',
-//     age:23
-// }
-
-// const AGE=23
-
-// class Animal{
-//     private name;
-//    public constructor(name){
-//        this.name=name
-//    }
-// }
 
 class Student {
-    fullName: string;
-    constructor(public firstName, public middleInitial, public lastName) {
-        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
-    }
+  fullName: string
+  constructor(public firstName:string, public middleInitial:string, public lastName:string) {
+    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
+  }
 }
 const enum Directions {
-    UP, DOWN, LEFT, RIGHT
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT
 }
 console.log(Directions.DOWN)
 interface Person {
-    firstName: string;
-    lastName: string;
+  firstName: string
+  lastName: string
 }
 function greeter(person: Person) {
-    return 'hello,' + person.firstName + ' ' + person.lastName
+  return 'hello,' + person.firstName + ' ' + person.lastName
 }
 let user = new Student('jack', 'M.', 'john')
 console.log(greeter(user))
