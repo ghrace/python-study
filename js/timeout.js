70bfe0e430e6d3cd67260c65bfe70ddd18f1266a
hongrong <ghrace@live.com>
2019-01-10 18:28:14 +0800

    110

---
+++ js/timeout.js
@@ -0,0 +1,11 @@
+function timeout(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+async function asyncPrint(value, ms) {
+  await timeout(ms)
+  console.log(value)
+}
+asyncPrint('hhh',1000)
